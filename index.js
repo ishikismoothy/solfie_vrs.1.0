bc39d96318e4dc4625f1f81c946d1062616f6a4d
Nick Westbrook <ntw3001@gmail.com>
2024-08-01 13:20:21 +0900

    hid API key in .env

---
+++ index.js
@@ -0,0 +1,4 @@
+// require('dotenv').config(); // Load environment variables from .env file
+
+// const apiKey = process.env.API_KEY; // Access the API key
+// console.log(`Your API Key is: ${apiKey}`); // Print the API key to the console
